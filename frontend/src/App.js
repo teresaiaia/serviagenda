be8b224b9f3af994e1b35253a13c24d55d9ffbca
teresaiaia <teresaferres@gmail.com>
2025-12-19 18:31:02 -0300

    Add ClientesView route to App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import CalendarioView from './components/CalendarioView';
 import EquiposView from './components/EquiposView';
+import ClientesView from './components/ClientesView';
 import './App.css';
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
         <Route path="/" element={<Navigate to="/calendario" replace />} />
         <Route path="/calendario" element={<CalendarioView />} />
         <Route path="/equipos" element={<EquiposView />} />
-        <Route path="/clientes" element={<Navigate to="/calendario" replace />} />
+        <Route path="/clientes" element={<ClientesView />} />
       </Routes>
     </Router>
   );
